2dd53f8b7229c3ae4a34ebb401709a7a6b5fd848
Marty Nelson <marty@martypdx.com>
2022-02-13 20:31:40 -0800

    use home dir on ci

--- __tests__/copy-file.test.js
+++ __tests__/copy-file.test.js
@@ -2,15 +2,17 @@ const fs = require('fs/promises');
 const path = require('path');
 const copyFile = require('../lib/copy-file');
 
-console.log('home', process.env.HOME);
-console.log('__dirname', __dirname);
-const COPY_FOLDER = path.join(__dirname, 'copy');
+const { CI, HOME } = process.env;
+const BASE_DIR = CI ? HOME : __dirname;
+const COPY_FOLDER = path.join(BASE_DIR, 'copy');
 const FILE_TO_COPY = path.join(COPY_FOLDER, 'file-to-copy.txt'); 
 const COPIED_FILE = path.join(COPY_FOLDER, 'copied-file.txt');
 
 describe('copy file', () => {
   beforeEach(async () => {
-    return fs.rm(COPIED_FILE, { force: true });
+    await fs.rm(COPY_FOLDER, { force: true, recursive: true });
+    await fs.mkdir(COPY_FOLDER, { recursive: true });
+    await fs.writeFile(FILE_TO_COPY, 'Copy me');
   });
 
   it('from src to dest', async () => {
